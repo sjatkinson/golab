c01e2859c8142541ad07d727fef349bbb9291138
sjatkinson <atkinson.steven@gmail.com>
2013-05-05 15:26:15 -0700

    modified cat to work more like unix cat - cats all files given otherwise stdin

--- apps/src/unix/cat/cat.go
+++ apps/src/unix/cat/cat.go
@@ -1,26 +1,42 @@
+/*
+   cat writes files to the stdout. 
+
+   If no arguments are given, cat copies the stdin to stdout. Otherwise each file
+   is writtne to stdout in order. Any errors are written to stderr, but processing is
+   not halted.
+*/
 package main
 
 import (
-   "os"
-   "io"
-   "flag"
-   "log"
-   "fmt"
+	"io"
+	"log"
+	"os"
 )
 
+// catFile copies a file to Stdout
+func catFile(f *os.File) error {
+	_, err := io.Copy(os.Stdout, f)
+	if err != nil {
+		return err
+	}
+	err = os.Stdout.Sync()
+	return err
+}
+
 func main() {
-   flag.Parse()
-   file, err := os.Open(flag.Arg(0))
-   if err != nil {
-      log.Fatal(err)
-   }
-   copied, err := io.Copy(os.Stdout, file)
-   if err != nil {
-      log.Fatal(err)
-   }
-   err = os.Stdout.Sync()
-   if err != nil {
-      log.Fatal(err)
-   }
-   fmt.Printf("copied %d\n", copied)
+	if len(os.Args) == 1 {
+		err := catFile(os.Stdin)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		for _, arg := range os.Args[1:] {
+			file, err := os.Open(arg)
+			if err != nil {
+                                 logger := log.New(os.Stderr, "", 0)
+				logger.Println(err)
+			}
+			catFile(file)
+		}
+	}
 }

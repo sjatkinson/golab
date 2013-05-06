9bcd76537ec6a953bcdd44d9fd81aa3164854656
sjatkinson <atkinson.steven@gmail.com>
2013-05-05 19:15:28 -0700

    clean up ls

--- apps/src/unix/ls/ls.go
+++ apps/src/unix/ls/ls.go
@@ -4,29 +4,27 @@
 package main
 
 import (
-       "io";
-       "fmt";
-       "os";
-       "flag";
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
 )
-
 
 // TODO: command line handling
 // -- -l
 // -- -a
 // -- -r
 func main() {
-  flag.Parse();
-  var dir string = ".";
-  if flag.NArg() > 0 {
-     dir = flag.Args()[0];
-  }
-  dirs, err := io.ReadDir(dir);
-  if len(dirs) == 0 {
-     fmt.Printf("Can not open directory: err=%s\n", err.String());
-     os.Exit(1);
-  }
-  for i := 0; i < len(dirs); i++ {
-     fmt.Printf("%s\n",dirs[i].Name);
-  }
+	flag.Parse()
+	var dir string = "."
+	if flag.NArg() > 0 {
+		dir = flag.Args()[0]
+	}
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Fatal("Error reading dir: ", err)
+	}
+	for _, f := range files {
+		fmt.Printf("%s\n", f.Name())
+	}
 }
